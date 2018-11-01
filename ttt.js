164273bf3580345d78bada29270ef127a96d25c7
CJ Horlebein <chrishorlebein@gmail.com>
2018-11-01 14:33:12 -0400

    making attempts to resolve TTT

--- ttt.js
+++ ttt.js
@@ -9,13 +9,21 @@ function renderTttLayout() {
     `
     renderTttGame(3);
 }
+var currGame;
+function resetTtt() {
+    currGame = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+}
 
 function renderTttGame(size) {
     var tttBoard = "";
     for (var i = 0; i < size; i++) {
         tttBoard += `<div class="w-25 text-center d-flex mx-auto justify-content-between">`
         for (var j = 0; j < size; j++) {
-            tttBoard += `<button class="ttt-btn w-25 m-2"></button>`
+            tttBoard += `<button id="${'' + i + j}" class="ttt-btn w-25 m-2">${currGame[i][j]}</button>`
         }
         tttBoard += `</div>`
     }
@@ -26,12 +34,16 @@ var playTtt = function () {
     tttButtons = Array.from(document.getElementsByClassName('ttt-btn'));
     tttButtons.forEach(position => {
         position.addEventListener('click', function () {
-            position.innerHTML = "X";
+            currGame[0][0] = "X";
+            console.log(this.id);
         });
     });
+    renderTttGame(3);
+    console.log(currGame);
 }
 var tttButton = document.getElementById('ttt');
 tttButton.addEventListener('click', function () {
+    resetTtt();
     renderTttLayout();
     playTtt();
 });
